b766e74e6989b03b6a2a70f2e0be45aeebb84b37
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-15 15:43:55 -0700

    Feat: add another method request and name file names accordingly

---
+++ alchemyu-json-activity/getBalance.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const axios = require('axios');
+
+//URL provided in your Alchemy.com dashboard
+const ALCHEMY_URL = process.env.ALCHEMY_URL;
+
+axios.post(ALCHEMY_URL, {
+  jsonrpc: "2.0",
+  id: 1,
+  method: "eth_getBalance",
+  params: [
+    "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045", // block 46147
+    "latest"  // retrieve the full transaction object in transactions array
+  ]
+}).then((response) => {
+  console.log(response.data.result);
+});
